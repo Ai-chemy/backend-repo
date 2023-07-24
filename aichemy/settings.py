bd2f0e048303c95f20a913892fe8d16c5374221f
JHPark02 <applestrawberryg@gmail.com>
2023-07-24 11:37:52 +0900

    feature: Modify database setting

--- aichemy/settings.py
+++ aichemy/settings.py
@@ -136,34 +136,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'aichemy.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         # 데이터베이스 이름
-#         'NAME': 'user',
-#         'USER' : "jh",
-#         # root 비밀번호
-#         'PASSWORD' : "jh3548",
-#         'HOST' : '127.0.0.1',
-#         'PORT': '3306'
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'user',
-        'USER': 'jh',
-        'PASSWORD': 'jh3548',
-        'HOST': 'postgresql',
-        'PORT': '5432',
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_USER_PASSWORD"),
+        'HOST': config("DATABASE_HOST"),
+        'PORT': config("DATABASE_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
